ed3c9b4c91fb5fc52ffba9e0c4ac6c085e7160ab
stephanjin <stephanjin99@gmail.com>
2025-04-15 19:31:47 -0700

    update vite-config to enable every component can use primevue

--- client_vue/src/main.ts
+++ client_vue/src/main.ts
@@ -1,5 +1,22 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import PrimeVue from 'primevue/config'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(PrimeVue, 
+    {
+        theme: {
+            // preset: AudioParam,
+            // options: {
+            //     prefix: 'p',
+            //     darkModeSelctor: 'system',
+            //     cssLayer: false
+            // },
+            // Unstyled mode instructs the components not to add any built-in style classes so that they can be styled using custom css or libraries like Tailwind and Bootstrap. 
+            unstyled: true
+        }
+    }
+);
+
+app.mount('#app')
