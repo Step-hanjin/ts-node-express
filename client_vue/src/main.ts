b5b740af79b5952c1f90b22a502e80ad464563ae
stephanjin <stephanjin99@gmail.com>
2025-04-15 19:43:17 -0700

    setup primevue in main.ts

--- client_vue/src/main.ts
+++ client_vue/src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
+import PrimeVue from  "primevue/config"
 import './style.css'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(PrimeVue);
+app.mount('#app')
