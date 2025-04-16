871dc8ef7ebcc5e8ab9b6c201ff05a3baf6f4235
stephanjin <stephanjin99@gmail.com>
2025-04-16 00:12:19 -0700

    create headbar

--- client_vue/src/main.ts
+++ client_vue/src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import PrimeVue from  "primevue/config"
+import Aura from '@primeuix/themes/aura'
+
 import './style.css'
 import App from './App.vue'
 
@@ -7,15 +9,16 @@ const app = createApp(App);
 app.use(PrimeVue, 
     {
         theme: {
-            // preset: AudioParam,
+            preset: Aura,
             // options: {
             //     prefix: 'p',
             //     darkModeSelctor: 'system',
             //     cssLayer: false
             // },
-            // Unstyled mode instructs the components not to add any built-in style classes so that they can be styled using custom css or libraries like Tailwind and Bootstrap. 
-            unstyled: true
-        }
+        },
+        // Unstyled mode instructs the components not to add any built-in style classes so that they can be styled using custom css or libraries like Tailwind and Bootstrap. 
+        // ripple: true,
+        // unstyled: true
     }
 );
 
